3b1b2b473c50c9547760827a0f93ab83f450f2d7
Kewei Zhang <kewei.zhang@kasna.com.au>
2020-10-22 14:47:32 +1100

    add sh to trigger .sh file

--- bin/terraform-apply.sh
+++ bin/terraform-apply.sh
@@ -5,7 +5,7 @@ set -exuo pipefail
 echo "Running on $BRANCH_NAME branch"
 
 terraform_apply () {
-	bin/terraform-init.sh
+	./bin/terraform-init.sh
 	cd env/${ENV}
 	terraform plan -out ${APP}-${ENV}.tfstate
 	terraform apply -auto-approve ${APP}-${ENV}.tfstate
