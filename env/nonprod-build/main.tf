7b10be1c256f99b3bedf275970a557c88681e43a
Kewei Zhang <kewei.zhang@kasna.com.au>
2020-10-26 00:04:25 +1100

    test

--- env/nonprod-build/main.tf
+++ env/nonprod-build/main.tf
@@ -7,3 +7,22 @@ module bootstrap-cloudbuild {
   build_env         = var.build_env
 }
 
+# Create Module CI Cloudbuild trigger for unit-testing based on feature branch
+resource google_cloudbuild_trigger module_dry_run {
+  provider    = google-beta
+  for_each    = fileset(path.module, "*")
+  description = "module ${each.value} - dry run"
+  project     = var.build_project_id
+  github {
+    owner = "kewei5zhang"
+    name  = "terraform-gcp-module"
+    push {
+      branch = "feature/${dirname(each.value)}*"
+    }
+  }
+  filename      = "cloudbuild-dry-run.yaml"
+  substitutions = merge(var.substitution_vars, { _MODULE = each.value })
+  included_files = [
+    "${dirname(each.value)}/module/**",
+  ]
+}
